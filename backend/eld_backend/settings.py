6a169a75022c0f19de17254f162fb3d6505616fd
mostafam7md <mostafamohamed1907@gmail.com>
2025-12-13 16:08:40 +0200

    modification for production

--- backend/eld_backend/settings.py
+++ backend/eld_backend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "dev-secret-key-change-me"
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
@@ -67,3 +67,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Dev CORS
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+  "https://spotter-task.vercel.app",
+]
